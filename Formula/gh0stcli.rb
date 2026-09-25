b19e5b18ae5093bb99b34ef600da9cc382cdd74c
gh0stservice CI <ci@gh0stservice.com>
2026-09-25 16:06:04 +0000

    gh0stcli v0.7.0

--- Formula/gh0stcli.rb
+++ Formula/gh0stcli.rb
@@ -1,26 +1,26 @@
 class Gh0stcli < Formula
   desc "Client and MCP server for the gh0stcloud managed Kubernetes platform"
   homepage "https://gh0stservice.com/en/gh0stcloud/self-service"
-  version "0.6.0"
+  version "0.7.0"
   license "LGPL-3.0-or-later"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.6.0/gh0stcli_0.6.0_darwin_arm64.tar.gz"
-      sha256 "62ae628bcd01183fef106d9c02ed4c91272cd0237e33fbd23bbce71c8b51aef0"
+      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.7.0/gh0stcli_0.7.0_darwin_arm64.tar.gz"
+      sha256 "65716d0cd0793c65431216b13e1d0030bab7d3135d3079f15c720d8641251f53"
     else
-      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.6.0/gh0stcli_0.6.0_darwin_amd64.tar.gz"
-      sha256 "ed3278717a7cb741471965c5a670be906a4de58a864c68de98c0401afd6d3fe8"
+      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.7.0/gh0stcli_0.7.0_darwin_amd64.tar.gz"
+      sha256 "316052211216d70a8f1abdfa2bead67ee9ca09ec3cbd373fe85cad446ab76d54"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.6.0/gh0stcli_0.6.0_linux_arm64.tar.gz"
-      sha256 "83e1f04f32e4c86dc98a3fceb9ba37c09acbecd7fd3f437e33c1d315c714547e"
+      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.7.0/gh0stcli_0.7.0_linux_arm64.tar.gz"
+      sha256 "299ca4f718e295650e747297160b0abfe3b483a07f150e6355e16606dee99901"
     else
-      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.6.0/gh0stcli_0.6.0_linux_amd64.tar.gz"
-      sha256 "41d4d748bf74b98495faa77c19568e964f76737ca3c7e701dd8b7aa0d0e3c185"
+      url "https://nbg1.your-objectstorage.com/gh0stservice-updates-vb4/gh0stcli/releases/v0.7.0/gh0stcli_0.7.0_linux_amd64.tar.gz"
+      sha256 "4d5d9e832cb21b92d8ab6baef7644d2d0a511db23a7f6b2da13b93427fe53bd7"
     end
   end
 
